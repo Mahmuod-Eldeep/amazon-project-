e1a0f1b93ed46d6e399ed74e24f3f43042b4a1d2
Mahmuod <mahmoudeldeep114@gmail.com>
2024-12-16 19:46:49 +0200

    Delete products from cart when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export const cart = [{
+export let cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
 },
@@ -36,6 +36,20 @@ export function showAddMessage(addedToCartMessage) {
     addedToCartMessage.timeoutId = setTimeout(() => {
         addedToCartMessage.style.opacity = 0;
     }, 1500);
+
+}
+
+export function removeFromCart(productId) {
+    const newCart = [];
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+    // cart = cart.filter(product => product.productId !== productId); the same thing 
+    console.log(cart);
 
 }
 
