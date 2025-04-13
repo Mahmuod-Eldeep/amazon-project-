cf4a69e2a3f8d36e5209c6d658a27b630e942faf
Mahmuod-Eldeep <mahmuodeldeep114@gmail.com>
2025-04-13 23:29:06 +0200

    Convert loadProducts from XHP to fetch

--- data/products.js
+++ data/products.js
@@ -60,8 +60,29 @@ class Clothing extends Product {
 
 export let products = [];
 
+export function loadProductsFetch() {
+ const promise =  fetch(
+  "https://supersimplebackend.dev/products"
+ ).then((respone) => {
+    return respone.json();
+  }).then((productDetails) => {
+     products = productDetails.map((productDetails) => {
+        if (productDetails.type === "clothing") {
+           return new Clothing(productDetails);
+         }
+         return new Product(productDetails);
+        });
+        console.log('loaded products');
+      
+    });
+    return promise;
+}
 
-
+/*
+loadProductsFetch().then(() => {
+  console.log('next step');
+});
+*/
 
 export function loadProducts(fun) {
   const  xhr  = new XMLHttpRequest();
@@ -74,8 +95,8 @@ xhr.addEventListener("load", () => {
   
      return new Product(productDetails);
     });
+    console.log('loaded products');
   fun();
-  console.log('loaded products');
 })
 
 
